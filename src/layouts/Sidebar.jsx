9332c48569f37306df279c68445595c1a2087d9c
Luke Sontrop <luke@bluesaltlabs.com>
2023-09-02 09:35:55 -0700

    Updates sidebar links so they are stored in an array for more efficient modification.

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,42 +1,45 @@
 import { NavLink } from "react-router-dom";
 
+const sidebarLinks = [
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    to: "/contacts",
+    title: "Contacts",
+  },
+  {
+    to: "/people",
+    title: "People",
+  },
+];
+
 
 function Sidebar() {
 
-    const SidebarLink = ({ to, title }) => (
-      <NavLink
-        to={to}
-        className={
-          `block px-2 py-3 font-medium ` +
-          `text-blue-600 dark:text-blue-500 hover:text-blue-200 hover:bg-slate-500`
-        }
-      >
+  const SidebarLink = ({ to, title }) => (
+    <NavLink
+      to={to}
+      className={
+        `block px-2 py-3 font-medium rounded-sm ` +
+        `text-blue-600 dark:text-blue-500 hover:text-blue-200 hover:bg-slate-500`
+      }
+    >
       { title }
     </NavLink>
   )
 
-  return (
-    
-  
+  return (  
     <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
       <div className="sticky top-0 p-4 w-full">
 
         <ul className="flex flex-col overflow-hidden bg-slate-800 p-4 rounded-md shadow-md">
-
-         <li>
-            <SidebarLink
-              to="/"
-              title="Home"
-            />
-          </li>
-          
-          <li>
-            <SidebarLink
-              to="/people"
-              title="People"
-            />
-          </li>
-          
+          {sidebarLinks.map((linkData, linkKey) => (
+            <li key={`link-${linkKey}`}>
+              <SidebarLink {...linkData} />
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
